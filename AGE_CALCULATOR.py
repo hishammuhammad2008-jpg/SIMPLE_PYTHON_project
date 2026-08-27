49d69e287e01bb52d5e6af54e12f660b418f398b
adm <hishammuhammad2008@gmail.com>
2026-08-27 20:57:45 +0530

    AGE_CALCULATOR.py

---
+++ AGE_CALCULATOR.py
@@ -0,0 +1,7 @@
+birth_year=int(input("ENTER YOUR BIRTH MOUNTH : "))
+current_year=int(input("ENTER THE CURRENY YEAR :"))
+age=current_year-birth_year
+print("YOUR CURRENT AGE IS ",age)
+#birth_year,current_year,age are variables 
+#int used because we want integer number only that why int used here
+#we use simple math problem to find age
