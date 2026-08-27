a90c5e86ce20bff621adc250976425a8588fffde
adm <hishammuhammad2008@gmail.com>
2026-08-27 20:32:38 +0530

    SIMPLE_INPUT_PROGRAM

---
+++ SIMPLE_INPUT_PROGRAM.py
@@ -0,0 +1,3 @@
+name=input("WHAT IS YOUR NAME ? ")#name is variable and "WHAT IS YOUR NAME ?" printing comment
+fav_colour=input("what is your fav color ? ")# same like above
+print(name," likes ",fav_colour ,"color")# out put eg:john  likes  blue color
