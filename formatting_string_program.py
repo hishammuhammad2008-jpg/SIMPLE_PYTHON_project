fdb3868c01a0626d21a319ad428926c5eb2f5edd
adm <hishammuhammad2008@gmail.com>
2026-08-28 10:57:35 +0530

    formatting_string_program

---
+++ formatting_string_program.py
@@ -0,0 +1,5 @@
+#print this using formatted string (first_name ['second_name'] is coder)
+first_name=str(input("ENTER YOUR FIRST NAME : "))
+second_name=str(input("ENTER YOUR SECOND NAME : "))
+msg=f"{first_name} {[second_name]} is coder"
+print(msg)
