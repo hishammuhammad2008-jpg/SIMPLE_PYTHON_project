4b310cfc1321d38a261b776a29b30b14bd940ca2
adm <hishammuhammad2008@gmail.com>
2026-08-29 19:55:27 +0530

    arithmetic_operator.py

---
+++ arithmetic_operator.py
@@ -0,0 +1,29 @@
+#1)Create a program that takes two numbers from the user and prints:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+value1=int(input("ENTER A NUMBER  ONE : "))
+value2=int(input("ENTER A NUMBER  TWO : "))
+print("ADDITION : ",value1+value2)
+print("SUBTRACTION : ",value1-value2)
+print("MULTIPLICATION : ",value1*value2)
+print("DIVISION : ",value1/value2)
+
+#2)A person has ₹500 and spends ₹175. Write a program to calculate how much money is less
+total_amount=500
+spending_amount=175
+print("THE AMONT LEFT : ",total_amount-spending_amount)
+
+#3)A rectangle has:
+
+# length = 20
+# width = 8
+# Calculate its:
+# Area
+# Perimeter
+length=20
+width=8
+print("AREA OF RECTANGLE : ",length*width)
+print("PERIMETER OF RECTANGLE : ",(length*2)+(2*width))
+
